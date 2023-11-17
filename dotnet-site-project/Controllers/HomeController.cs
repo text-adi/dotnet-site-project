d20e001847bc06de1e839e398035cff5225af708
text-adi <alosha123456789049@gmail.com>
2023-11-17 21:51:41 +0200

    Added all task for l2

--- dotnet-site-project/Controllers/HomeController.cs
+++ dotnet-site-project/Controllers/HomeController.cs
@@ -15,13 +15,10 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        _logger.LogInformation("Open Main page");
         return View();
     }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
